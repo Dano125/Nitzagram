4b435bb1c4b23f36e24d515635a6e660a5dcd4ce
Dano125 <dsustret@gmail.com>
2025-01-22 19:45:16 +0200

    i added: init, add_like and add_comment methods to Post class.

--- classes/Post.py
+++ classes/Post.py
@@ -8,9 +8,18 @@ class Post:
     """
     A class used to represent post on Nitzagram
     """
-    def __init__(self): #TODO: add parameters
-        #TODO: write me!
-        pass
+    def __init__(self, username, location, description, likes_counter, comments):
+        self.username = username
+        self.location = location
+        self.description = description
+        self.likes_counter = likes_counter
+        self.comments = comments
+
+    def add_like(self):
+        self.likes_counter += 1
+
+    def add_comment(self, text):
+        self.comments.append(text)
 
     def display(self):
         """
