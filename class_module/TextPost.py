4fe5aab6178225bbc6f954715dededf3f9052c2b
rezero567 <itayrabayev2000@gmail.com>
2025-02-01 23:25:23 +0200

    Update TextPost.py

--- class_module/TextPost.py
+++ class_module/TextPost.py
@@ -6,5 +6,20 @@ from class_module import Post
 
 
 class TextPost(Post):
-    # TODO: write me! draw background and create text then override the super().display() method
-    pass
+    def __init__(self, username, location, description):
+        super().__init__(username, location, description)
+
+    def display(self):
+        super().display()
+
+        background_color = (255, 255, 255)  
+        text_color = (0, 0, 0)  
+        font = pygame.font.Font(None, 36) 
+        pygame.draw.rect(screen, background_color, (POST_X_POS, POST_Y_POS, POST_WIDTH, POST_HEIGHT))
+
+        text_surface = font.render(self.description, True, text_color)
+
+        text_rect = text_surface.get_rect(center=(POST_X_POS + POST_WIDTH // 2, POST_Y_POS + POST_HEIGHT // 2))
+
+        screen.blit(text_surface, text_rect)
+
